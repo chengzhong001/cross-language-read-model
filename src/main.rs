cc9a2d27d140e00dda0fb41c9dca5781f2bf501f
azhong <zczwzw@163.com>
2022-12-16 19:30:46 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 use tch::{Tensor, jit};
 
 
-fn my_test() {
-    let t = Tensor::of_slice(&[3, 2, 1, 2]);
-    t.print();
+// fn my_test() {
+//     let t = Tensor::of_slice(&[3, 2, 1, 2]);
+//     t.print();
+// }
+
+#[warn(dead_code)]
+fn run_infer() {
+
+    // code-1
+    let image_value = Tensor::randn(&[1,1,8, 8], tch::kind::FLOAT_CPU);
+
+    // code-2
+    let model = jit::CModule::load(
+        "/Users/azhong/Documents/Project/cross-language-read-model/model/digit.jit"
+    ).unwrap();
+    let ouput = model.forward_ts(&[image_value]).unwrap();
+    ouput.print();
+
 }
 
-
 fn main() {
-    my_test();
+    // my_test();
+    run_infer();
+    // println!("Hello, world!");
 }
